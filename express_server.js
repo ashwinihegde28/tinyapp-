4eaf1433c1aa20100fab80ff71ee0e34a9abf569
ashwinihegde28 <ashwinihegde28@gmail.com>
2023-03-21 22:24:53 -0400

    Redid the tINY url and fixed some erros

--- express_server.js
+++ express_server.js
@@ -1,7 +1,6 @@
-const { Template } = require("ejs");
 const express = require("express");
 const app = express();
-const PORT = 8080;
+const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
@@ -10,15 +9,9 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com",
 };
 
-app.use(express.urlencoded({ extended: true }));
+// Routing
 app.get("/", (req, res) => {
   res.send("Hello!");
-});
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.get("/urls", (req, res) => {
@@ -26,39 +19,13 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.status(404).send("Ok"); // Respond with 'Ok' (we will replace this)
-  shortId = generateRandomString();
-  console.log("shortId => " + generateRandomString());
-  urlDatabase["shortId"] = req.body["longURL"];
-  console.log(urlDatabase);
-});
-
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
-});
-
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: req.params.longURL };
+  const templateVars = {
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id],
+  };
   res.render("urls_show", templateVars);
 });
-
-app.get("/u/:id", (req, res) => {
-  const longURL = req.body["longURL"];
-  res.redirect(longURL);
-});
-
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
-//Need to clarify
-
-function generateRandomString() {
-  let chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  let res = "";
-  for (let i = 0; i < 6; i++) {
-    res += chars[Math.floor(Math.random() * chars.length)];
-  }
-  return res;
-}
