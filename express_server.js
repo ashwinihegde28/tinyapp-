99c7981bdb8f395cc7e096f2ee50629169f0ac62
ashwinihegde28 <ashwinihegde28@gmail.com>
2023-03-22 01:50:54 -0400

    code for URL Shortening (Part 1)

--- express_server.js
+++ express_server.js
@@ -4,6 +4,9 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+//middleware which will translate, or parse the body
+app.use(express.urlencoded({ extended: true }));
+
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
@@ -19,6 +22,23 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+//We're going to need two new routes: a GET route to render the urls_new.ejs template (given below) in the browser,
+//to present the form to the user; and a POST route to handle the form submission.
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+//when the form is submitted, it will make a request to POST /urls, and the body will contain one URL-encoded name-value pair with the name longURL
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  const shortURL = generateRandomString();
+  console.log(shortURL);
+  const longURL = req.body["longURL"];
+  console.log(longURL);
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = {
     id: req.params.id,
@@ -26,6 +46,17 @@ app.get("/urls/:id", (req, res) => {
   };
   res.render("urls_show", templateVars);
 });
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+//This function returns a string of 6 random alphanumeric characters that can be used as an "unique" Short URL id
+function generateRandomString() {
+  let chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let res = "";
+  for (let i = 0; i < 6; i++) {
+    res += chars[Math.floor(Math.random() * chars.length)];
+  }
+  return res;
+}
