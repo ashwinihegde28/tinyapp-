99f57a1ab3d15638fe363adeb8b4ed1451b1d191
ashwinihegde28 <ashwinihegde28@gmail.com>
2023-03-23 13:49:04 -0400

    Code for Registration Errors, Registering New Users and litlle code modification done

--- express_server.js
+++ express_server.js
@@ -13,92 +13,19 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com",
 };
 
-// Routing
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
-app.get("/urls", (req, res) => {
-  const templateVars = { username: req.cookies["username"], urls: urlDatabase };
-
-  res.render("urls_index", templateVars);
-});
-
-//We're going to need two new routes: a GET route to render the urls_new.ejs template (given below) in the browser,
-//to present the form to the user; and a POST route to handle the form submission.
-
-app.get("/urls/new", (req, res) => {
-  const templateVars = {
-    username: req.cookies["username"],
-  };
-  res.render("urls_new");
-});
-
-//when the form is submitted, it will make a request to POST /urls, and the body will contain one URL-encoded name-value pair with the name longURL
-app.post("/urls", (req, res) => {
-  // Respond with "OK" by setting the status code to 200.
-  const shortURL = generateRandomString();
-  const longURL = req.body["longURL"];
-  // add this newly added url into the urlDatabase.
-  urlDatabase[shortURL] = longURL;
-  res.status(200).redirect(`/urls/${shortURL}`);
-});
-
-app.get("/urls/:id", (req, res) => {
-  const templateVars = {
-    username: req.cookies["username"],
-    id: req.params.id,
-    longURL: urlDatabase[req.params.id],
-  };
-  res.render("urls_show", templateVars);
-});
-
-///u/
-app.get("/u/:id", (req, res) => {
-  //We redirected to long url
-  console.log(req.params.id);
-  console.log("urlDatabase =>", urlDatabase);
-  res.status(200).redirect(urlDatabase[req.params.id]);
-});
-
-//For editing existing url
-app.post("/urls/:shortURL", (req, res) => {
-  const shortURL = req.params.shortURL;
-  const longURL = req.body.longURL;
-  console.log(longURL);
-  urlDatabase[shortURL] = longURL;
-  res.redirect(`/urls`);
-});
-
-//For deleting the url
-app.post("/urls/:shortURL/delete", (req, res) => {
-  const shortURL = req.params.shortURL;
-  delete urlDatabase[shortURL];
-  res.redirect(`/urls`);
-});
-
-//Login Page functionality
-app.post("/login", (req, res) => {
-  console.log("req.body.username # ", req.body.username);
-  res.cookie(`username`, req.body.username);
-  res.redirect(`/urls`);
-});
-
-//Logout
-app.post("/logout", (req, res) => {
-  res.clearCookie("username");
-  res.redirect("/urls");
-});
-
-// Display the register form
-app.get("/register", (req, res) => {
-  const templateVars = { user: null };
-  res.render("register", templateVars);
-});
-
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
+//Users to store and access the users in the app
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
 
 //This function returns a string of 6 random alphanumeric characters that can be used as an "unique" Short URL id
 function generateRandomString() {
@@ -109,3 +36,131 @@ function generateRandomString() {
   }
   return res;
 }
+
+//create a function to look up if email already exist
+const getUserByEmail = (email) => {
+  for (const user in users) {
+    if (email === users[user].email) {
+      return user;
+    }
+  }
+  return null;
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello!"); //this will eventually change
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = {
+    user: users[req.cookies["user_id"]],
+    urls: urlDatabase,
+  };
+
+  res.render("urls_index", templateVars);
+});
+
+// render create new url form
+app.get("/urls/new", (req, res) => {
+  const templateVars = {
+    user: users[req.cookies["user_id"]],
+  };
+  res.render("urls_new", templateVars);
+});
+
+// render show page
+app.get("/urls/:id", (req, res) => {
+  const templateVars = {
+    user: users[req.cookies["user_id"]],
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id],
+  };
+  res.render("urls_show", templateVars);
+});
+
+// redirect to corresponding longurl
+app.get("/u/:id", (req, res) => {
+  res.status(200).redirect(urlDatabase[req.params.id]);
+});
+
+//adding brand new url to DB
+app.post("/urls", (req, res) => {
+  // Respond with "OK" by setting the status code to 200.
+  const shortURL = generateRandomString();
+  const longURL = req.body["longURL"];
+  // add this newly added url into the urlDatabase.
+  urlDatabase[shortURL] = longURL;
+  res.status(200).redirect(`/urls/${shortURL}`);
+});
+
+//editing existing url
+app.post("/urls/:id", (req, res) => {
+  const shortURL = req.params.id;
+  const longURL = req.body.longURL;
+  urlDatabase[shortURL] = longURL;
+  res.redirect("/urls");
+});
+
+//deleting the url
+app.post("/urls/:id/delete", (req, res) => {
+  const shortURL = req.params.id;
+  delete urlDatabase[shortURL];
+  res.redirect(`/urls`);
+});
+
+//render login form
+app.get("/login", (req, res) => {
+  const templateVars = { user: null };
+  res.render("login", templateVars);
+});
+
+//login existing user
+app.post("/login", (req, res) => {
+  res.cookie("user_id", req.body.username);
+  res.redirect("/urls");
+});
+
+//Logout
+app.post("/logout", (req, res) => {
+  res.clearCookie("user_id");
+  res.redirect("/urls");
+});
+
+//render register form
+app.get("/register", (req, res) => {
+  const templateVars = { user: null };
+  res.render("register", templateVars);
+});
+
+// add new user
+app.post("/register", (req, res) => {
+  //Extract the email and password from the form
+  // req.body (body-parser) will get the info from our form
+  //generate the random user id
+
+  const randomUserId = generateRandomString();
+  // es6 syntax
+  const { email, password } = req.body;
+  //if empty strings --> response = 404 statuscode
+  if (email === "" || password === "") {
+    console.log("404 Empty email");
+    res
+      .status(404)
+      .send("Either email or password is empty, enter a valid one.");
+  } else if (getUserByEmail(email) != "null") {
+    console.log("already exists!");
+    res.status(404).send("User Alredy Exisits");
+  }
+  users[randomUserId] = {
+    id: randomUserId,
+    email: email,
+    password: password,
+  };
+
+  res.cookie("user_id", randomUserId);
+  res.redirect("/urls");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
